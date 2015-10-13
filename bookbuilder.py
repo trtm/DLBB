e952c6c40f110f43fb4599446020badf2fc13d80
Dietrich Trautmann <dtrtrtm@gmail.com>
2015-10-13 15:50:55 +0200

    process input

--- bookbuilder.py
+++ bookbuilder.py
@@ -19,3 +19,25 @@ user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) \
     AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
 headers = { 'User-Agent' : user_agent }
 
+#-------------------------------------------------------------------------------
+# 0. Input
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-w', '--webpage', type=str , help='specify webpage')
+parser.add_argument('-s', '--seconds', type=int, default=60, help='how many seconds to wait after printing')
+args = parser.parse_args()
+
+#
+webpage = args.webpage
+print( 'webpage:', webpage)
+
+#
+seconds = args.seconds
+print( 'seconds:', seconds)
+
+#
+proc = subprocess.Popen(['echo $PWD', ''], stdout=subprocess.PIPE, shell=True)
+out, err = proc.communicate()
+PWD = str(out).replace('\n', '')
+print( 'PWD:', PWD)
+
