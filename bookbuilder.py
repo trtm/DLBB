9f0da628dfefa4fca433cebb33ee7fbd89e194af
Dietrich Trautmann <dtrtrtm@gmail.com>
2015-10-14 09:02:50 +0200

    cleaned up

--- bookbuilder.py
+++ bookbuilder.py
@@ -6,7 +6,6 @@
 # author          : https://github.com/trtm
 # date            : 2015 10 12
 # version         : 0.1
-# licence         : WTFPL, http://www.wtfpl.net
 
 from __future__ import print_function
 import os
@@ -16,66 +15,43 @@ import subprocess
 from time import sleep
 from bs4 import BeautifulSoup
 
-user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) \
-    AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
-headers = { 'User-Agent' : user_agent }
-
-#-------------------------------------------------------------------------------
 # 0. Input
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-w', '--webpage', type=str , help='specify webpage')
 parser.add_argument('-s', '--seconds', type=int, default=60, help='how many seconds to wait after printing')
 args = parser.parse_args()
 
-#
 webpage = args.webpage
-print( 'webpage:', webpage)
 
-#
 seconds = args.seconds
-print( 'seconds:', seconds)
 
-#
 proc = subprocess.Popen(['echo $PWD', ''], stdout=subprocess.PIPE, shell=True)
 out, err = proc.communicate()
 PWD = str(out).replace('\n', '')
-print( 'PWD:', PWD)
 
-#-------------------------------------------------------------------------------
 # 1. Use wget to get webpage content
+os.system(u'wget -c -r -l1 http://www.iro.umontreal.ca/~bengioy/DLbook/')
 
-os.system(u'wget -c -r -l1 %s' % webpage )
-
-#-------------------------------------------------------------------------------
 # 2. Create soup object
-
 path = PWD + webpage.replace('http://', '/')
-print( 'path:', path )
-
 url = 'file://' + path + 'index.html'
-print( 'url:', url )
 
-#
-req = urllib2.Request( url, None, headers )
+req = urllib2.Request( url )
 response = urllib2.urlopen(req)
 soup = BeautifulSoup(response, 'html.parser')
 
-#-------------------------------------------------------------------------------
 # 3. Print html files with Firefox
-
 output_pdfs = []
 for item in soup.find_all('a'):
     item = str(item)
     item = item.replace('<a href="', '')
     item = item.replace('</a>', '')
-    link, name = item.split('">')
-    link = link.replace('http://', '')
+    item = item.split('">')
+    item = item[0].replace('http://', '')
 
-    if not 'version' in link and '.html' in link:
-        orig = path + link
+    if not 'version' in item and '.html' in item:
+        orig = path + item
         output_pdf = orig.replace('.html', '.pdf')
-
         output_pdfs.append( output_pdf )
 
         if not os.path.isfile( output_pdf ):
@@ -84,18 +60,11 @@ for item in soup.find_all('a'):
         else:
             print( 'already printed:', output_pdf )
 
-#-------------------------------------------------------------------------------
 # 4. Create argument of available single PDFs to merge
-
 file_arg = ''
 for f in output_pdfs:
     file_arg += f
     file_arg += ' '
 
-#-------------------------------------------------------------------------------
 # 5. Merge PDFs with pdftk
-
 os.system(u'pdftk %s cat output ~/Desktop/DLB.pdf' % file_arg )
-
-
-
